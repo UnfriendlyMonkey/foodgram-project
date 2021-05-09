3a220331d2b6a475d343599c46fcfef926cca927
Andrey Sereda <andreysereda@ya.ru>
2021-05-09 19:44:19 +0300

    made part of Recipe model and intermediary RecipeIngredient model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+	model = RecipeIngredient
+	extra = 1
+
+
+class RecipeAdmin(admin.ModelAdmin):
+	inlines = [RecipeIngredientInline]
 
 
 admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient)
--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class Ingredient(models.Model):
@@ -7,3 +11,20 @@ class Ingredient(models.Model):
 
 	def __str__(self):
 		return f"{self.name}, {self.unit}"
+
+
+class Recipe(models.Model):
+	title = models.CharField(max_length=250)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	description = models.TextField()
+	cooking_time = models.PositiveIntegerField()
+	ingredient = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+
+	def __str__(self):
+		return self.title
+
+
+class RecipeIngredient(models.Model):
+	recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+	ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+	quantity = models.PositiveIntegerField()
