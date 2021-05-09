966ef91e7be3f2f3cacf37a7ac1f46cc67a1d7d4
Andrey Sereda <andreysereda@ya.ru>
2021-05-09 18:41:27 +0300

    made Ingredients model. applied first migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+	name = models.CharField(max_length=250)
+	unit = models.CharField(max_length=125)
+
+	def __str__(self):
+		return f"{self.name}, {self.unit}"
